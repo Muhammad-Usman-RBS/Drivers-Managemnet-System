20b9395a42f7d8450e2bcd8220683981b5b133d1
mtlcontrol13-ship-it <mtlcontrol13@gmail.com>
2026-02-26 11:48:34 +0500

    Fix multer

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,7 +1,9 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multerStorageCloudinary from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
 import path from "path";
+
+const CloudinaryStorage = multerStorageCloudinary;
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
